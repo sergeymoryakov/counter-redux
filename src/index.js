8e72e1fa4ddef3495b018ac93f516afe5f4c0e69
Sergey Moryakov <sergey@moryakov.com>
2024-01-21 13:11:31 +0200

    set counter in vanilla js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
 import "./styles.css";
+
+const counter = document.getElementById("counter");
+const addBtnNode = document.getElementById("add");
+const subBtnNode = document.getElementById("sub");
+const asyncBtnNode = document.getElementById("async");
+const themeBtnNode = document.getElementById("theme");
+
+let state = 0;
+
+function render() {
+    counter.textContent = state.toString();
+}
+
+addBtnNode.addEventListener("click", () => {
+    state++;
+    render();
+});
+
+subBtnNode.addEventListener("click", () => {
+    state--;
+    render();
+});
+
+asyncBtnNode.addEventListener("click", () => {
+    setTimeout(() => {
+        state++;
+        render();
+    }, 2000);
+});
+
+themeBtnNode.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+});
+
+render();
